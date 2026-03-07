e8a7997e8fe841aadf2a5826b9e4735d2c5edb90
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-03-07 23:07:45 +0600

    js connected to issue.html

--- script/login.js
+++ script/login.js
@@ -2,15 +2,15 @@ const userName=document.getElementById('user-name');
 const userPassword=document.getElementById('user-password');
 const signinBtn=document.getElementById('sign-in');
 signinBtn.addEventListener('click', function(){
-    const userNameValue=userName.value
-    const userPasswordValue=userPassword.value
+    const userNameValue=userName.value;
+    const userPasswordValue=userPassword.value;
 if(userNameValue==="admin" && userPasswordValue==="admin123"){
-    alert('login Success')
-    window.location.assign("issue.html")
+    alert('login Success');
+    window.location.assign("issue.html");
 }
 else{
-    alert('Invalid User Name or Password')
-    return
+    alert('Invalid User Name or Password');
+    return;
 }
 
 })
